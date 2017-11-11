6d5f025a17072781a6b4d38a8441b5e735e8b5bd
YASH <yashwanthbkumar@gmail.com>
2017-11-11 20:05:18 +0530

    saving two fields to the database

--- src/main/java/com/amazonaws/lambda/s3submittions/test.java
+++ src/main/java/com/amazonaws/lambda/s3submittions/test.java
@@ -4,6 +4,7 @@ package com.amazonaws.lambda.s3submittions;
 //import org.json.simple.JSONObject;
 /*import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -14,6 +15,10 @@ import java.util.Iterator;
 import org.json.simple.JSONArray; 
 import org.json.simple.JSONObject; 
 import org.json.simple.parser.JSONParser; 
+
+
+
+
 
 
 
@@ -34,19 +39,26 @@ import java.util.List;
 
 public class test {
 	 public static void main(String[] args) throws SQLException {
+		 String dbURL  = "jdbc:mysql://admin.ct2n3gpfnz9q.ap-south-1.rds.amazonaws.com:3306/Javahome";
+	 		String dbUser = "admin"; 
+	 		String dbPass = "adminadmin";
+	 		Connection conn = null; 
+		
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
 
-	
+			Random r=new Random();
 	
 	 
 	//            Object obj = parser.parse(new FileReader("C:\\Users\\Yash\\git\\s3-submittions\\src\\main\\resources\\create-multiple.json"));
 String file="C:\\Users\\Yash\\git\\s3-submittions\\src\\main\\resources\\create-multiple.json";
 		 JSONParser parser = new JSONParser();
 		 try { 
-			 System.out.println("Reading JSON file from Java program"); 
+		 
 		 FileReader fileReader = new FileReader(file);
 		 JSONObject json = (JSONObject) parser.parse(fileReader);
 		 String title = (String) json.get("type");
-		 System.out.println("title: " + title);
+		 System.out.println("type: " + title);
 		 
 		 JSONObject jsonObject = (JSONObject) json;
          JSONArray slideContent = (JSONArray) jsonObject.get("issues");
@@ -56,26 +68,49 @@ String file="C:\\Users\\Yash\\git\\s3-submittions\\src\\main\\resources\\create-
          while (i1.hasNext()) {
             
          	JSONObject slide = (JSONObject) i1.next();
-             String name = (String)slide.get("baseQsip");
-             System.out.println(name);
+             String baseQsip = (String)slide.get("baseQsip");
+             /*Generating random number for offering_id*/
+             String offering_id_generate=r.nextSessionId();
+     		String sql = "insert into demo(offering_id,baseQsip) values (?,?)";	 
+    		PreparedStatement statement = conn.prepareStatement(sql);
+    		
+    		statement.setString(1,baseQsip);
+    		statement.setString(2,offering_id_generate);
+    		
+    		
+    		int row = statement.executeUpdate();
+    		if (row > 0) {
+    		    System.out.println(" saved");
+    		    
+    		    }
+
+             
+             
          }
 		 
-		 
-		 
-		 JSONArray characters = (JSONArray) json.get("issues"); 
-		 Iterator i = characters.iterator(); 
-		 System.out.println("characters: ");
-		
-		 while (i.hasNext()) 
-		 { 
-			 System.out.println(" " + i.next()); 
-			 
-
          
-		 }
+/*         JSONObject o=(JSONObject)jsonObject.get("offering");
+         
+         String offering_name = (String)o.get("legalDs");
+         
+         System.out.println(offering_name);
+*/          
+      
+         
+/*         JSONArray Main_issues = (JSONArray) jsonObject.get("issues"); 
+         for (int i = 0; i < Main_issues.size(); i++) {
+             
+             JSONArray sec_array = (JSONArray) new JSONArray().get(i);
+             System.out.println("====================///////////"+sec_array);
 
-		
-		 
+             for (int j = 0; j < sec_array.size(); j++) {
+            	    String secId = (String) sec_array.get(0);
+            	    System.out.println("Secid" + secId);
+            	}}
+         
+         */
+         
+         
 		 
 		 }
 		 catch (Exception ex) { ex.printStackTrace(); }
